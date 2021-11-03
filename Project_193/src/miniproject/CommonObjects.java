4fb764859221a1e26393fc517c1b2a390dcaf521
sunilreddyg <sunilreddy.gajjala@gmail.com>
2021-11-03 13:34:42 +0530

    Mini Project

---
+++ Project_193/src/miniproject/CommonObjects.java
@@ -0,0 +1,10 @@
+package miniproject;
+
+public interface CommonObjects 
+{
+	
+	String obj_login_email="//input[@id='txtUsername']";
+	String obj_login_password="//input[@id='txtPassword']";
+	String obj_login_Submit_button="//input[@id='btnLogin']";
+
+}
