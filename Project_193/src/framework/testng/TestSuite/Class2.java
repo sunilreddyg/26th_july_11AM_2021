65257aa0bbaac90997b3dbd209fe6fc9ea8a96bb
sunilreddyg <sunilreddy.gajjala@gmail.com>
2021-11-09 13:40:56 +0530

    Add files via upload

---
+++ Project_193/src/framework/testng/TestSuite/Class2.java
@@ -0,0 +1,35 @@
+package framework.testng.TestSuite;
+
+import org.testng.Reporter;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Class2 
+{
+	
+	@Test
+	public void tc004()
+	{
+		Reporter.log("tcoo4 executed");
+	}
+	
+	@Test
+	public void tc005()
+	{
+		Reporter.log("tcoo5 executed");
+	}
+	
+	
+	@Test
+	public void tc006()
+	{
+		Reporter.log("tcoo6 executed");
+	}
+	
+	@BeforeClass
+	public void beforeclass()
+	{
+		System.out.println("Class 2 preCondition is executed");
+	}
+ 
+}
